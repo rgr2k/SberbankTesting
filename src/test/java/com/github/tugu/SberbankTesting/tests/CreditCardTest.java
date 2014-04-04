e6f18440192411b9378109518b0d542d6acace4d
Denis Poznyak <tugu.by@gmail.com>
2014-04-04 21:18:26 +0300

    Updated formatting

--- src/test/java/com/github/tugu/SberbankTesting/tests/CreditCardTest.java
+++ src/test/java/com/github/tugu/SberbankTesting/tests/CreditCardTest.java
@@ -45,7 +45,8 @@ public class CreditCardTest {
 		// Проверяем наличие закладок
 		ArrayList<String> startPageTabsOnWebsite = getTabsOnStartPage();		
 		ArrayList<String> expectedStartPageTabs = getExpectedStartPageTabs(firstSheet);
-		Assert.assertEquals(startPageTabsOnWebsite, expectedStartPageTabs, "Закладки на главной странице не совпадают");
+		Assert.assertEquals(startPageTabsOnWebsite, expectedStartPageTabs,
+				"Закладки на главной странице не совпадают");
 		System.out.println("Список закладок проверен");
 		printLogDelimiter();
 		
@@ -56,9 +57,11 @@ public class CreditCardTest {
 		}
 		printLogDelimiter();
 		
-		// Переходим на закладку Выбрать карту  > кредитные карты  > универсальные кредитные карты  > премиальные карты
+		// Переходим на закладку Выбрать карту  > кредитные карты  >
+		// универсальные кредитные карты  > премиальные карты
 		Actions actions = new Actions(driver);
-		actions.moveToElement(driver.findElementByXPath("//a[text()[contains(.,'Выбрать')] and text()[contains(.,'карту')]]"));		
+		actions.moveToElement(driver.findElementByXPath("//a[text()[contains(.,'Выбрать')] "
+				+ "and text()[contains(.,'карту')]]"));		
 		actions.perform();
 		driver.findElementByXPath("(//a[text()[contains(.,'Премиальные карты')]])[2]").click();
 		System.out.println("Информация о кредитной карте открыта");
@@ -67,12 +70,18 @@ public class CreditCardTest {
 		// Проверяем правильность информации о кредитной карте
 		Sheet creditCardSheet = dataWorkbook.getSheet("CreditCard");
 		for (int i = 0; i < creditCardSheet.getRows(); i++) {
-			WebElement infoElement = driver.findElementByXPath(String.format("//table[@class='product-specific']//tr[%d]//td[1]", i+1));
-			WebElement valueElement = driver.findElementByXPath(String.format("//table[@class='product-specific']//tr[%d]//td[2]", i+1));
+			WebElement infoElement =
+					driver.findElementByXPath(String.format("//table[@class='product-specific']//tr[%d]//td[1]", i+1));
+			WebElement valueElement =
+					driver.findElementByXPath(String.format("//table[@class='product-specific']//tr[%d]//td[2]", i+1));
+			
 			String infoExpected = creditCardSheet.getCell(0, i).getContents();
 			String valueExpected = creditCardSheet.getCell(1, i).getContents();
-			Assert.assertEquals(infoElement.getText(), infoExpected, "Название параметра карты не совпадает");
-			Assert.assertEquals(valueElement.getText(), valueExpected, String.format("Значение параметра '%s' карты не совпадает", infoExpected));			
+			
+			Assert.assertEquals(infoElement.getText(), infoExpected,
+					"Название параметра карты не совпадает");
+			Assert.assertEquals(valueElement.getText(), valueExpected,
+					String.format("Значение параметра '%s' карты не совпадает", infoExpected));			
 		}		
 		System.out.println("Информация о кредитной карте проверена");
 		printLogDelimiter();
@@ -80,7 +89,8 @@ public class CreditCardTest {
 	
 	private ArrayList<String> getCreditLinksFromStartPage() {
 		ArrayList<String> result = new ArrayList<String>();
-		ArrayList<WebElement> links = new ArrayList<WebElement>(driver.findElementsByXPath("//b[text()='Кредиты']/../../../../div[1]//ul//a"));
+		ArrayList<WebElement> links = 
+				new ArrayList<WebElement>(driver.findElementsByXPath("//b[text()='Кредиты']/../../../../div[1]//ul//a"));
 		for (WebElement link : links) {
 			result.add(link.getText());			
 		}
